7a0cdb73a5c59f409c9526413accf9592fad8747
Harshad313D <hdexamples@gmail.com>
2026-05-12 14:25:26 +0530

    first

---
+++ Server.js
@@ -0,0 +1,38 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./Config/db.js";
+import cookieParser from "cookie-parser";
+
+
+import adminRoutes from "./Routes/adminAuthRoutes.js"
+import userRoutes from "./Routes/User/userAuthRoutes.js"
+import userMngRoutes from "./Routes/userRoutes.js"
+import adminProducts from "./Routes/admin-productRoutes.js"
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+    ],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    credentials: true,
+  }),
+);
+app.use(express.json());
+
+// admin routes
+app.use("/api/v1/admin", adminRoutes);
+app.use("/api/v1/admin/users", userMngRoutes);
+app.use("/api/v1/user", userRoutes);
+app.use("/api/v1/products", adminProducts);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port http://localhost:${PORT}`);
+});
